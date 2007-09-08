67a6cf68941330c383da3a4e1fd3205833ad733a
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-09-08 12:41:39 +0000

    split the R source of the package into several files, so they can be more easily copied to other places
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/convert@26953 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/marrayRaw_to_NChannelSet.R
@@ -0,0 +1,62 @@
+## Martin Morgan
+## 27 August, 2007
+## Modified from marrayRaw -> exprSet
+setAs("marrayRaw", "NChannelSet", function(from) {
+  
+    ## assayData
+    elts <- list(R=maRf(from), G=maGf(from))
+    if (length(maRb(from))>0)
+        elts[["Rb"]] <- maRb(from)
+    if (length(maGb(from))>0)
+        elts[["Gb"]] <- maGb(from)
+    assayData <-
+        do.call("assayDataNew",
+                c(storage.mode="lockedEnvironment", elts))
+    
+    ## phenoData, featureData
+    pData <- 
+        if (length(maInfo(maTargets(from)))>0) {
+            data=maInfo(maTargets(from))
+        } else {
+            data=data.frame(rep(0, ncol(from)))[,FALSE]
+        }
+    phenoData <- new("AnnotatedDataFrame", data=pData)
+    fData <- 
+        if (length(maInfo(maGnames(from)))>0) {
+            maInfo(maGnames(from))
+        } else {
+            data.frame(rep(0, nrow(from)))[,FALSE]
+        }
+    
+    if (!is.null(rownames(assayData[["R"]])))
+        row.names(fData) <- rownames(assayData[["R"]])
+    featureData <- new("AnnotatedDataFrame", data=fData)
+
+    ## experimentData
+    experimentData <- new("MIAME",
+                          other=list("::Converted from marrayRaw object"))
+    ## NChannelSet
+    obj <- new("NChannelSet",
+               assayData=assayData,
+               phenoData=phenoData,
+               featureData=featureData,
+               experimentData=experimentData)
+    
+    ## adjustments
+    if (!is.null(obj[["Names"]])) {
+        phenoData(obj)[["FileName",
+                        labelDescription="Source file name"]] <-
+                            sampleNames(obj)[[1]]
+        sampleNames(obj) <- obj[["Names"]]
+    }
+    lbls <- maLabels(maGnames(from))
+    if (length(lbls)==nrow(from)) {
+        if (any(duplicated(lbls)))
+            featureData(obj)[["maLabels",
+                              labelDescription="marrayRaw gene names"]] <-
+                                  lbls
+        else
+            featureNames(obj) <- lbls
+    }
+    obj
+})
